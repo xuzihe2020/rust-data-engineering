0a100b79912af89dc7eec9216208493c274c89a3
xuzihe2020 <zihex@tepper.cmu.edu>
2025-04-05 16:03:02 -0400

    Update vector_fruit_salad

--- vector_fruit_salad/src/main.rs
+++ vector_fruit_salad/src/main.rs
@@ -1,4 +1,30 @@
+//! This program creates a fruit salad by scrambling (shuffling) a list of fruit.
+//! A vector is a growable array. It can grow or shrink in size and is one of the most
+//! useful data structures in Rust. A vector is represented using the Vec<T> type.
+
+use rand::seq::SliceRandom;
+use rand::rng;
+
 fn main() {
-    println!("Hello, world!");
-    println!("Hello, vector world!");
+    let mut fruit: Vec<&str> = vec![
+        "apple",
+        "banana",
+        "cherry",
+        "date",
+        "elderberry",
+        "fig",
+        "grape",
+    ];
+
+    let mut rng: rand::prelude::ThreadRng = rng();
+    fruit.shuffle(&mut rng);
+    
+    // Print the (shuffled) fruit salad
+    for (i, item) in fruit.iter().enumerate() {
+        if i != fruit.len() - 1 {
+            print!("{}, ", item);
+        } else {
+            print!("{}", item);
+        }
+    }
 }

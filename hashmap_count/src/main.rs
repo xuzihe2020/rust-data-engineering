fef342d9da4086bd163f846234cf22f0b492fc97
xuzihe2020 <zihex@tepper.cmu.edu>
2025-04-05 21:52:36 -0400

    update hashmap_count project

--- hashmap_count/src/main.rs
+++ hashmap_count/src/main.rs
@@ -1,4 +1,30 @@
+//! This example code counts the frequency of each number in the vector.
+//!
+use std::collections::HashMap;
+
+fn logic(numbers: &Vec<i32>) -> Vec<(i32, i32)> {
+    let mut frequencies = HashMap::new();
+
+    for number in numbers {
+        let frequency: &mut i32 = frequencies.entry(number).or_insert(0);
+        *frequency += 1;
+    }
+
+    let mut result: Vec<(i32, i32)> = Vec::<(i32, i32)>::new();
+
+    for (num, frequency) in frequencies {
+        result.push((*num, frequency));
+    }
+
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
-    println!("Hello, hashmap world!");
+    let numbers: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 1, 2, 3, 1];
+    let result = logic(&numbers);
+    println!(
+        "The frequency of each number in the vector is: {:?}",
+        result
+    );
+    println!("The original vector is: {:?}", numbers);
 }
